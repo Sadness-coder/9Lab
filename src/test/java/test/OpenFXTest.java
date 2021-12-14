92f41a9f4dd17db9f38aef3aab4d3f1e99d34a3b
Nahagliivv <Rebesh.mark@gmail.com>
2021-12-14 14:36:17 +0300

    clean code

--- src/test/java/test/OpenFXTest.java
+++ src/test/java/test/OpenFXTest.java
@@ -2,8 +2,6 @@ package test;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.GeckoDriverInfo;
