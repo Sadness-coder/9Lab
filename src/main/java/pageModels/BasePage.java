e0dce20ff874b8b1890d0ed15777ee6399a9df87
Dmitriy Gradovets <dgradovec@gmail.com>
2021-12-15 22:40:37 +0300

    1212

--- src/main/java/pageModels/BasePage.java
+++ src/main/java/pageModels/BasePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public abstract class BasePage {
+public abstract class BasePage  {
 
     protected WebDriver driver;
 
